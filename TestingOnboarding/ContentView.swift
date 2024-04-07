7ebf0b24d1e9e1a9d26494c82e42a454e9044332
soleribbon <ivar202@gmail.com>
2024-04-07 14:48:53 +0200

    Initial Commit

---
+++ TestingOnboarding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestingOnboarding
+//
+//  Created by Ravi Heyne on 07/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestingOnboarding/TestingOnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingOnboardingApp.swift
+//  TestingOnboarding
+//
+//  Created by Ravi Heyne on 07/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingOnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
